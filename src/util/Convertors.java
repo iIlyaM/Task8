fa019be6a7d34bb3160926375c04f06b66725539
iIlyaM <71345635+iIlyaM@users.noreply.github.com>
2021-02-11 00:17:26 +0300

    Add files via upload

--- src/util/Convertors.java
+++ src/util/Convertors.java
@@ -9,7 +9,7 @@ public class Convertors {
 
     public static String[][] toStringArray(List<String> list) {
         String[][] arr = new String[list.size()][];
-        for(int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             arr[i] = list.get(i).split("\\s*(\\s|,|!|\\.)\\s*");
         }
         return arr;
@@ -17,8 +17,8 @@ public class Convertors {
 
     public static String[][] toStringArray(int[][] arr) {
         String[][] array = new String[arr.length][arr[0].length];
-        for(int i = 0; i < arr.length; i++){
-            for(int j = 0; j < arr[0].length; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
                 array[i][j] = String.valueOf(arr[i][j]);
             }
         }
@@ -27,7 +27,7 @@ public class Convertors {
 
     public static int[][] toIntArray(String[][] arr) {
         int[][] array = new int[arr.length][arr[0].length];
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 array[i][j] = Integer.parseInt(arr[i][j]);
             }
@@ -37,7 +37,7 @@ public class Convertors {
 
     public static int[][] toIntArray(Object[][] arr) {
         int[][] array = new int[arr.length][arr[0].length];
-        for(int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
                 array[i][j] = Integer.parseInt(String.valueOf(arr[i][j]));
             }
@@ -49,11 +49,9 @@ public class Convertors {
         DefaultTableModel dtm = (DefaultTableModel) table.getModel();
         int nRow = dtm.getRowCount(), nCol = dtm.getColumnCount();
         Object[][] tableData = new Object[nRow][nCol];
-        for (int i = 0 ; i < nRow ; i++)
-            for (int j = 0 ; j < nCol ; j++)
-                tableData[i][j] = dtm.getValueAt(i,j);
+        for (int i = 0; i < nRow; i++)
+            for (int j = 0; j < nCol; j++)
+                tableData[i][j] = dtm.getValueAt(i, j);
         return tableData;
     }
-
-
 }
